6fbb543b7ea054819a7dc68e53bf6cdb63f0fa0c
James Nicolas <jamesnicolas@me.com>
2019-11-27 17:52:56 -0500

    problem 25

---
+++ 25 Reverse Nodes In K Group/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseUntil(head, tail *ListNode) *ListNode {
+	if head != nil && head.Next != nil {
+		reversed := head
+		toDo := head.Next
+		reversed.Next = tail
+		tmp := toDo.Next
+		for toDo != tail {
+			tmp = toDo.Next
+			toDo.Next = reversed
+			reversed = toDo
+			toDo = tmp
+		}
+		return reversed
+	}
+	return head
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	kthNode := head
+	for i := 0; i < k; i++ {
+		if kthNode != nil && kthNode.Next != nil {
+			kthNode = kthNode.Next
+		} else {
+			return kthNode
+		}
+	}
+	tmp := head
+	head = reverseUntil(head, kthNode)
+	tmp.Next = reverseKGroup(kthNode, k)
+	return head
+}
+
+func cons(i int, n *ListNode) *ListNode {
+	return &ListNode{i, n}
+}
+
+func main() {
+	a := cons(1, cons(2, cons(3, cons(4, cons(5, nil)))))
+	b := reverseKGroup(a, 1)
+	fmt.Println(b)
+	fmt.Println(b.Next)
+	fmt.Println(b.Next.Next)
+	fmt.Println(b.Next.Next.Next)
+	fmt.Println(b.Next.Next.Next.Next)
+}
