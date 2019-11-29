94692e5ce808c7a81df4d9658da9ac3649295c5b
James Nicolas <jamesnicolas@me.com>
2019-11-29 00:08:38 -0500

    wip

--- 56 Merge Intervals/main.go
+++ 56 Merge Intervals/main.go
@@ -9,6 +9,19 @@ type intervalCol struct {
 	intervals [][]int
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func (in intervalCol) Less(i, j int) bool {
 	if in.intervals[i][0] == in.intervals[j][0] {
 		return in.intervals[i][1] < in.intervals[j][1]
@@ -20,6 +33,9 @@ func (in intervalCol) Len() int {
 }
 func (in intervalCol) Swap(i, j int) {
 	in.intervals[i], in.intervals[j] = in.intervals[j], in.intervals[i]
+}
+
+func mergeTwo(a, b []int) []int {
 }
 
 func merge(intervals [][]int) [][]int {
