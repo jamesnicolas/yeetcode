bf1f6a983e6141a778994712e0ddc10a21e99833
James Nicolas <jamesnicolas@me.com>
2019-11-28 22:45:35 -0500

    first attempt unomptimized

---
+++ 238 Product of Array Except Self/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func productExceptSelf(nums []int) []int {
+	length := len(nums)
+	if length == 0 {
+		return []int{}
+	}
+	if length == 1 {
+		return []int{1}
+	}
+	answer := make([]int, length)
+	leftProduct := make([]int, length)
+	rightProduct := make([]int, length)
+	rightProduct[length-1] = 1
+	for i := length - 2; i >= 0; i-- {
+		rightProduct[i] = rightProduct[i+1] * nums[i+1]
+	}
+	leftProduct[0] = 1
+	answer[0] = rightProduct[0]
+	for i := 1; i < length; i++ {
+		leftProduct[i] = leftProduct[i-1] * nums[i-1]
+		answer[i] = rightProduct[i] * leftProduct[i]
+	}
+	return answer
+}
+
+func main() {
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))
+}
